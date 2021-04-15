19370015f3dd18ee81a9ec4c8b6a29762ebe6fe5
Artem-Renard <82466743+Artem-Renard@users.noreply.github.com>
2021-04-15 18:49:09 +0300

    Добавлены классы, объекты и конструкторы объектов

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,23 +7,22 @@ public class MyFirstProgram {
       hello ("SOVA");
       hello ("PUH");
 
-      double d = 5;
-      System.out.println("Площадь квадрата со стороной " + d +" = " + area (d));
+      Square s = new Square(3);
+      System.out.println("Площадь квадрата со стороной " + s.l +" = " + area (s));
 
-      double a = 4;
-      double b = 5;
-      System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area (a, b));
+      Rectangle r = new Rectangle(44, 55);
+      System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area (r));
     }
 
     public static void hello (String somebody) {
       System.out.println("Hello, " + somebody + "!");
     }
 
-    public static double area (double l) {
-      return l * l;
+    public static double area (Square s) {
+      return s.l * s.l;
     }
 
-    public static double area (double a, double b) {
-      return a * b;
+    public static double area (Rectangle r) {
+      return r.a * r.b;
     }
 }
