c5120e640ecc4467693749c7df7021509bf645ea
Artem-Renard <82466743+Artem-Renard@users.noreply.github.com>
2021-04-21 19:25:00 +0300

    Добавлен тест PointTest

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/point/PointTest.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.sandbox.point;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.stqa.pft.sandbox.Square;
+
+public class PointTest {
+  @Test
+  public void testArea () {
+    Point t1 = new Point (0,0);
+    Point t2 = new Point (0,3);
+    t1.distance(t2);
+    Assert.assertEquals(t1.distance(t2),3 );
+  }
+
+}
