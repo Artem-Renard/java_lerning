6d857cf22282b0a14dc1a1d05dd155c8d1dfc4f6
Artem-Renard <82466743+Artem-Renard@users.noreply.github.com>
2021-04-25 10:54:35 +0300

    Изменения проверок

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
@@ -15,17 +15,7 @@ public class Equation {
 
     double d = b * b - 4 * a * c;
 
-    if (a == 0) {
-      if (b == 0) {
-        if (c == 0) {
-          n = -1;
-        } else {
-          n = 0;
-        }
-      } else {
-        n = 1;
-      }
-    } else {
+    if (a != 0) {
       if (d > 0) {
         n = 2;
       } else {
@@ -35,8 +25,19 @@ public class Equation {
           n = 0;
         }
       }
-    }
+
+    } else if (b != 0) {
+      n = 1;
+
+    } else if (c != 0) {
+      n = 0;
+      
+      } else {
+      n = -1;
+      }
+
   }
+
     public int rootNumber () {
       return n;
     }
