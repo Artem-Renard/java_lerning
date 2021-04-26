97875a925d0012f24b02ea1c765b28bc1ea7c29e
Artem-Renard <82466743+Artem-Renard@users.noreply.github.com>
2021-04-26 10:50:36 +0300

    Реализована проверка и обеспечение выполнения предусловий

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -8,10 +8,13 @@ public class GroupModificationTests extends TestBase {
   @Test
   public void testGroupModification () {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("NewTest1", null, null));
+    app.getGroupHelper().fillGroupForm(new GroupData("NewTest1", "NewTest2", "NewTest3"));
     app.getGroupHelper().submitGroupModification();
-    app.getNavigationHelper().gotoHomePage();
+    app.getNavigationHelper().gotoGroupPage();
   }
 }
