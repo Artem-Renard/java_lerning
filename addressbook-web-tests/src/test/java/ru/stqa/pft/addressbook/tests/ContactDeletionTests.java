2762dd2c58f40e1da36b1bbac93b4864a3706f1e
Artem-Renard <82466743+Artem-Renard@users.noreply.github.com>
2021-05-03 16:46:36 +0300

    Добавлена проверка сравнения списков в теста Contact/GroupDeletionsTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -27,5 +27,8 @@ public class ContactDeletionTests extends TestBase {
     app.getContactHelper().goHomePage();
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() - 1);
+
+    before.remove(before.size() - 1);
+    Assert.assertEquals(before, after);
   }
 }
