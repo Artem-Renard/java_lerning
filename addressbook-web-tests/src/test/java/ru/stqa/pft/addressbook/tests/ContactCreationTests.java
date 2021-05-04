ae12fa9cd1898e053914ab5891a009e37baffdc5
Artem-Renard <82466743+Artem-Renard@users.noreply.github.com>
2021-05-04 21:08:52 +0300

    Добавлены лямбда-выражения в Contact/GroupCreationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -13,19 +13,13 @@ public class ContactCreationTests  extends TestBase  {
   @Test
   public void testContactCreation() {
     List<ContactData> before = app.getContactHelper().getContactList();
-    ContactData contact = new ContactData("TestName", "TestLastName", "TestHomeTelephone", "TestE-mail@test.ru", "[none]");
+    ContactData contact = new ContactData("TestName1", "TestLastName1", "TestHomeTelephone1", "Test1-mail@test.ru", "[none]");
     app.getContactHelper().createContact(contact, true);
     app.getContactHelper().goHomePage();
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    int max = 0;
-    for (ContactData c : after) {
-      if (c.getId() > max) {
-        max = c.getId();
-      }
-    }
-    contact.setId(max);
+    contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(contact);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -14,18 +15,12 @@ public class GroupCreationTests extends TestBase {
     app.getGroupHelper().goToGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getNavigationHelper().gotoGroupPage();
-    GroupData group = new GroupData("test1", "test2", "test3");
+    GroupData group = new GroupData("test2", "test3", "test4");
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    int max = 0;
-    for (GroupData g: after) {
-      if (g.getId() > max) {
-        max = g.getId();
-      }
-    }
-    group.setId(max);
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
