a1fa93708845704482bbf456317be617e3b60ddc
Artem-Renard <82466743+Artem-Renard@users.noreply.github.com>
2021-06-07 10:52:23 +0300

    Добавлены комментарии к тестам

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -21,20 +21,32 @@ public class RegistrationTests extends TestBase {
 
   @Test
   public void testRegistration() throws MessagingException, IOException {
+    // идентификатор для создания новых пользователей при каждом запуске теста
     long now = System.currentTimeMillis();
     String user = String.format("user%s", now);
     String password = "password";
     String email = String.format("user%s@localhost.localdomain", now);
+    // регистрация пользователя
     app.registration().start(user, email);
+    // получение почты с внутреннего сервера
     List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+    // извлечение ссылки для подтверждения регистрации из писем отправленных на email
     String confirmationLink = findConfirmationLink(mailMessages, email);
+    // подтверждение регистрации пользователя
     app.registration().finish(confirmationLink, password);
+    // проверка входа с помощью протокола HTTP
     assertTrue(app.newSession().login(user, password));
   }
 
   private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
+    // ищем письмо отправленное на email используя filter передаваемый предикатом
+    // (функция возвращающая или ложь или истину) (m) -> m.to.equals(email) остануться только те сообщения в потоке,
+    // которые были отправленны на email и берем первое сообщение
     MailMessage mailMessage = mailMessages.stream().filter((m) -> m.to.equals(email)).findFirst().get();
+    // с помощью библиотеки регулярных выражений ищем
     VerbalExpression regex = VerbalExpression.regex().find("http://").nonSpace().oneOrMore().build();
+    // применили регулярное выражение к тексту письма и вернули
+    // т.е. возвращается тот кусок текста, который соответствует построенному regex (регулярному выражению)
     return regex.getText(mailMessage.text);
   }
 
